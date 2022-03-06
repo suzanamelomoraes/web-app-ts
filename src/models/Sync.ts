d6b42714679cc5eb7dfda2c3d0ab6c947e102e22
Suzana Melo <suzanamelojor@gmail.com>
2022-03-07 09:09:56 +1300

    web-app: create a generic class to handle fetch and save methods from User.ts

---
+++ src/models/Sync.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosPromise } from "axios";
+
+// .then((response: AxiosResponse): void => {
+//     this.set(response.data)});
+
+interface HasId {
+  id?: number;
+}
+
+export class Sync<T extends HasId> {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
