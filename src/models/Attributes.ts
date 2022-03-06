fde93f1ba7698da92ac0a5507fb234b6843a71a8
Suzana Melo <suzanamelojor@gmail.com>
2022-03-07 11:32:00 +1300

    web-app: refactor get method and implement generic constraint to extend its type to each type provided by T accordingly

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,8 +1,16 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): string | number {
-    return this.data[propName];
+  // using generics/Generic Constraint with method definitions to avoid issues that may be caused by using unions
+
+  //   get(key: string): string | number {
+  //     return this.data[key];
+  //   }
+
+  // <K extends keyof T> type of K can only be one of the keys of T
+  // T[K] - Take the T and lookup for the K type inside it
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
