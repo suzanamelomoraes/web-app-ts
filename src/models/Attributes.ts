5ae01bb81306342d5c89a6fc1be412cd440ab446
Suzana Melo <suzanamelojor@gmail.com>
2022-03-08 21:20:38 +1300

    web-app: make get an arrow function to fix the context, this issue

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -9,9 +9,9 @@ export class Attributes<T> {
 
   // <K extends keyof T> type of K can only be one of the keys of T
   // T[K] - Take the T and lookup for the K type inside it
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
-  }
+  };
 
   set(update: T): void {
     Object.assign(this.data, update);
