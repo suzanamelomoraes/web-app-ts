bc9ce52d4e3d5ff2c426bc276a704aeb08b62812
Suzana Melo <suzanamelojor@gmail.com>
2022-03-19 18:02:13 +1300

    web-app: add comments

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -3,6 +3,8 @@ type Callback = () => void;
 export class Eventing {
   events: { [key: string]: Callback[] } = {};
 
+  // Arrow function here to avoid 'this' issue in this case
+
   on = (eventName: string, callback: Callback): void => {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
