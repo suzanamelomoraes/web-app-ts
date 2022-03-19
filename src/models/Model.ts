cec0e39ee8b4a3d6414ee23dd770c8dc3172c53c
Suzana Melo <suzanamelojor@gmail.com>
2022-03-19 18:10:55 +1300

    web-app: create class model and include interfaces for attributes, sync and events

---
+++ src/models/Model.ts
@@ -0,0 +1,21 @@
+import { AxiosPromise } from "axios";
+
+type Callback = () => void;
+
+interface ModelAttributes<T> {
+  set(update: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface Events {
+  on(eventName: string, callback: Callback): void;
+  trigger(eventName: string): void;
+}
+
+export class Model {}
