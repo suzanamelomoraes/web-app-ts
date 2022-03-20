92c346778d0b65c0a99305bd668b6070626401e7
Suzana Melo <suzanamelojor@gmail.com>
2022-03-20 16:01:36 +1300

    web-app: import and initalize eventing to sign up to the others components when an event happens

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,5 +1,16 @@
+import { Eventing } from "./Eventing";
 import { User } from "./User";
 
 export class Collection {
   models: User[] = [];
+  events: Eventing = new Eventing();
+
+  //use get as an accessor to events methods
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
 }
