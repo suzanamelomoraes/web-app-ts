8d00cc283ca72af8725485eeb1e6f64767a78e75
Suzana Melo <suzanamelojor@gmail.com>
2022-03-21 14:39:00 +1300

    web-app: create static method to instantiate Collection when it is called

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from "./Model";
 import { Attributes } from "./Attributes";
 import { ApiSync } from "./ApiSync";
 import { Eventing } from "./Eventing";
+import { Collection } from "./Collection";
 
 export interface UserProps {
   id?: number;
@@ -19,6 +20,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new ApiSync<UserProps>(rootUrl),
       new Eventing()
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
