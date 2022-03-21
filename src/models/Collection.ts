63cf1ca196ccb9c9bfa5c951b65603f9897d510c
Suzana Melo <suzanamelojor@gmail.com>
2022-03-21 14:28:57 +1300

    web-app: include a fetch method and refactor class to make it reusable to any type of data by using generic types

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,9 +1,14 @@
+import axios, { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
-import { User } from "./User";
 
-export class Collection {
-  models: User[] = [];
+// Refactor Collection to be reusable to any type of data
+// Replacing type User, which includes a type UserProps, we included the generic T
+// plus a second generic called K, which represents, in this example, the User looking model
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
+
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   //use get as an accessor to events methods
   get on() {
@@ -12,5 +17,16 @@ export class Collection {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      response.data.forEach((value: K) => {
+        // const user = User.buildUser(value);
+        // this.models.push(user);
+        this.models.push(this.deserialize(value));
+      });
+      this.trigger("change");
+    });
   }
 }
